1345abb5298f3aa1f4c5cdfaa94632713b7fbd8d
Cmunoz <cmunozm6@miumg.edu.gt>
2026-03-06 14:13:56 -0600

    creacion del modelo asignacion_curso

---
+++ universidad/Models/asignacion_curso/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from universidad.Models.Curso.models import Curso
+from universidad.Models.catedratico.models import Catedratico
+
+
+class AsignacionCurso(models.Model):
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    catedratico = models.ForeignKey(Catedratico, on_delete=models.CASCADE)
+    horario = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.curso} - {self.catedratico} ({self.horario})"
+
+    class Meta:
+        db_table = 'asignacion_curso'
+
