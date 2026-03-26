7bd70ab4393e7b20192c3ed270891138732a8b36
Cmunoz <cmunozm6@miumg.edu.gt>
2026-03-26 00:26:38 -0600

    ajustes en el modelo catedratico para añadir campos descriptivos

--- universidad/Models/catedratico/models.py
+++ universidad/Models/catedratico/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 class Catedratico(models.Model):
-    nombre = models.CharField(max_length=100)
-    especialidad = models.CharField(max_length=100, blank=True)
+    first_name   = models.CharField(max_length=100, verbose_name="Nombre")
+    last_name    = models.CharField(max_length=100, verbose_name="Apellido")
+    especialidad = models.CharField(max_length=100, blank=True, verbose_name="Especialidad")
+    email        = models.EmailField(unique=True, verbose_name="Correo electrónico")
+    phone        = models.CharField(max_length=20, blank=True, verbose_name="Teléfono")
 
     def __str__(self):
-        return self.nombre
+        return f"{self.first_name} {self.last_name}"
 
     class Meta:
-        db_table = 'catedratico'
-
+        db_table  = 'catedratico'
+        ordering  = ['last_name', 'first_name']
+        verbose_name        = 'Catedrático'
+        verbose_name_plural = 'Catedráticos'
--- universidad/Models/catedratico/views.py
+++ universidad/Models/catedratico/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Catedratico
@@ -6,16 +5,17 @@ from .models import Catedratico
 class CatedraticoListView(ListView):
     model = Catedratico
     template_name = 'catedratico/list.html'
+    context_object_name = 'object_list'
 
 class CatedraticoCreateView(CreateView):
     model = Catedratico
-    fields = ['nombre', 'especialidad']
+    fields = ['first_name', 'last_name', 'especialidad', 'email', 'phone']
     template_name = 'catedratico/form.html'
     success_url = reverse_lazy('catedratico:list')
 
 class CatedraticoUpdateView(UpdateView):
     model = Catedratico
-    fields = ['nombre', 'especialidad']
+    fields = ['first_name', 'last_name', 'especialidad', 'email', 'phone']
     template_name = 'catedratico/form.html'
     success_url = reverse_lazy('catedratico:list')
 
